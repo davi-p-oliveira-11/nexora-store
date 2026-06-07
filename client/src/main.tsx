32edb4bd408dd74d2da4061b669a6f9374856ca6
Davi P. Oliveira <dpo.dev11@gmail.com>
2026-06-07 13:00:40 -0300

    feat(server): configure webhooks and backend services

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { ClerkProvider } from '@clerk/react'
+
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={clerkPubKey}>
+       <App />
+    </ClerkProvider> 
   </StrictMode>,
 )
