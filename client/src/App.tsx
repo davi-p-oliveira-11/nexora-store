89e5981ee6d5603b48b3a29fa99ed7b6186ec8d5
Davi P. Oliveira <dpo.dev11@gmail.com>
2026-06-15 19:50:48 -0300

    feat(client): implement order video call experience

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,7 @@ import { SentryDemoPage } from './pages/SentryDemoPage';
 import OrderDetailPage from './pages/OrderDetailPage';
 import OrderSummaryPage from './pages/OrderSummaryPage';
 import OrderChatPage from './pages/OrderChatPage';
+import OrderVideoPage from './pages/OrderVideoPage';
 
 function App() {
   const { isLoaded, isSignedIn } = useAuth();
@@ -27,11 +28,16 @@ function App() {
         <Route path="/checkout/return" element={<CheckoutReturnPage />}/>
         <Route path="/demo-sentry"  element={<SentryDemoPage />}/>
 
+        <Route
+          path="/orders/:id/call"
+          element={isSignedIn ? <OrderVideoPage /> : <Navigate to={"/"} replace />}
+        />
+
+
         {/* Nested Routes */}
         <Route path="/orders/:id" element={<OrderDetailPage />}>
            <Route index element={<OrderSummaryPage />}/>
            <Route path="chat" element={<OrderChatPage />} />
-
 
         </Route>
       </Routes>
