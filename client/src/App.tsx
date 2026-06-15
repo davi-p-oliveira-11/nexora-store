8c18a953f38d20560d99e5c0658fc7b4518688e5
Davi P. Oliveira <dpo.dev11@gmail.com>
2026-06-15 11:51:07 -0300

    feat(client): add checkout return page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { Routes, Route, Navigate } from 'react-router';
 import Homepage from './pages/Homepage';
 import CartPage from './pages/CartPage';
 import OrdersPage from './pages/OrdersPage';
+import CheckoutReturnPage from './pages/CheckoutReturnPage';
 
 function App() {
   const { isLoaded, isSignedIn } = useAuth();
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<Homepage />}/>
         <Route path="/cart" element={<CartPage />}/>
         <Route path="/orders" element={isSignedIn ? <OrdersPage /> : <Navigate to={"/"} replace />} />
+        <Route path="/checkout/return" element={<CheckoutReturnPage />}/>
       </Routes>
     </Layout>
   )
